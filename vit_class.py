ad388739b945f9a483c9e3d6d4f0cbe22768ad62
George Skiadas <skiadas.giorgos04@gmail.com>
2023-05-24 16:32:02 +0000

    Upload New File

---
+++ vit_class.py
@@ -0,0 +1,23 @@
+from transformers import ViTModel, ViTConfig
+import torch.nn as nn
+from draft import model_checkpoint
+
+class ViT(nn.Module):
+
+  def __init__(self, config=ViTConfig(), num_labels=500, 
+               model_checkpoint=model_checkpoint):
+
+        super(ViT, self).__init__()
+
+        self.vit = ViTModel.from_pretrained(model_checkpoint, add_pooling_layer=False)
+        self.classifier = (
+            nn.Linear(config.hidden_size, num_labels) 
+        )
+
+  def forward(self, x):
+
+    x = self.vit(x)['last_hidden_state']
+    # Use the embedding of [CLS] token
+    output = self.classifier(x[:, 0, :])
+
+    return output

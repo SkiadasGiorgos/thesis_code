532c04ed1f406b3eaa190622654779b57f43199c
George Skiadas <skiadas.giorgos04@gmail.com>
2023-05-24 16:31:42 +0000

    Upload New File

---
+++ transforms.py
@@ -0,0 +1,25 @@
+from torchvision import transforms
+
+# Values were found here https://huggingface.co/google/vit-base-patch16-224-in21k
+mean = [0.5, 0.5, 0.5]
+std  = [0.5, 0.5, 0.5]
+
+def create_transforms(train=True):
+  if train:
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((224, 224), antialias=True),
+        transforms.Normalize(mean=mean,
+                             std=std
+                             ),
+    ])
+  else:
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean,
+                             std=std
+                             ),
+        transforms.Resize((224, 224), antialias=True),
+    ])
+
+  return transform

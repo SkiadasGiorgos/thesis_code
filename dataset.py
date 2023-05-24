c03265209ec373e79a6297e37975ccbc77f2d7fa
George Skiadas <skiadas.giorgos04@gmail.com>
2023-05-24 16:32:28 +0000

    Upload New File

---
+++ dataset.py
@@ -0,0 +1,24 @@
+import glob
+from PIL import Image
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import Dataset
+from transforms import create_transforms
+
+
+class MyDataset(Dataset):
+    def __init__(self, path, transform):
+        self.files = glob.glob(path)
+        print(type(self.files))
+        self.transform = create_transforms(transform)
+        self.labels = [filepath.split('/')[-2] for filepath in self.files]
+        self.encoder = LabelEncoder()
+        self.encoder.fit(self.labels)
+        self.labels = self.encoder.transform(self.labels)
+    def __getitem__(self, item):
+        image = self.files[item]
+        image = Image.open(image)
+        image = self.transform(image)
+        label = self.labels[item]
+        return image, label
+    def __len__(self):
+        return len(self.files)

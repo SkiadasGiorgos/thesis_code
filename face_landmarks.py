75b236422a096dfee6c9e0928b2bef2b60577cba
SkiadasGiorgos <skiadas.giorgos04@gmail.com>
2023-06-27 17:16:19 +0300

    Correct mistake regarding patch center initialization

--- face_landmarks.py
+++ face_landmarks.py
@@ -4,23 +4,70 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 from transforms import transforms
+import glob
+import pickle
+from patches_dims import calculate_landmark_patches, centroids,patch_centers
+from matplotlib.patches import Rectangle
+
+save_path = "landmarks.pkl"
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 mtcnn = MTCNN(margin=20,keep_all=True,post_process=False,device=device)
+image_list = glob.glob("/nas2/ckoutlis/DataStorage/vggface2/data/test/*/*.jpg")
 
-frame = Image.open("/nas2/ckoutlis/DataStorage/vggface2/data/test/n008655/0558_01.jpg")
-frame.resize([224,224])
-boxes,probs,landmarks = mtcnn.detect(frame,landmarks=True)
-
-print(landmarks)
+frame = Image.open('/nas2/ckoutlis/DataStorage/vggface2/data/test/n006497/0029_02.jpg')
+frame = frame.resize([224,224])
+_,_,landmarks = mtcnn.detect(frame,landmarks=True)
 
 
+anchor_points = []
+for i in range(0,224,32):
+    for j in range(0,224,32):
+        anchor_points.append([i,j])
+
+patches,center_patches = calculate_landmark_patches(landmarks)
+# print(patches)
+# print(landmarks)
+# print(patch_centers)
 # Landmarks in random photo
-# fig,ax = plt.subplots()
-# ax.imshow(frame)
-# ax.axis('off')
+fig,ax = plt.subplots()
+ax.imshow(frame)
+ax.axis('off')
+iter =0
+for anchor in anchor_points:    
+    rectangle = Rectangle(anchor,32,32,linewidth=1, edgecolor='r', facecolor='none')
+    ax.add_patch(rectangle)
+for  landmark in landmarks:
+    ax.scatter(landmark[:,0],landmark[:,1],s=8)
+for center in center_patches:
+    ax.scatter(center[0],center[1],s=8,c='w')
 
-# for  landmark in landmarks:
-#     ax.scatter(landmark[:,0],landmark[:,1],s=8)
-# fig.savefig("/home/skiadasg/thesis_code/thesis_code/rand.png")
+fig.savefig("/home/skiadasg/thesis_code/thesis_code/rand.png")
+
+# landmark_list = []
+# c =0 
+# for image in image_list:
+#     frame = Image.open(image)
+#     frame = frame.resize([224,224])
+#     _,_,landmarks = mtcnn.detect(frame,landmarks=True)
+#     landmark_patch = calculate_landmark_patches(landmarks)
+#     temp_dict = {"image":image,
+#                  "landmarks":landmark_patch}
+#     with open(save_path,'ab') as h:
+#         pickle.dump(temp_dict,h)
+#     c+=1
+#     if c==10:
+#         break 
+
+
+# objects = []
+# with (open(save_path, "rb")) as openfile:
+#     while True:
+#         try:
+#             objects.append(pickle.load(openfile))
+#             # objects.append(torch.load(save_path,map_location=torch.device('cpu')))
+#         except EOFError:
+#             break
+
+# print(objects)
--- patches_dims.py
+++ patches_dims.py
@@ -12,21 +12,32 @@ y = np.flip(y)
 x = np.arange(0,image_width,patch_size)
 centroids = []
 
-for i in x:
-    for j in y:
+for j in y:
+    for i in x:
         centroids.append([i+square_centroid_dist,j-square_centroid_dist])
 
 patch_centers = []
 for index,value in enumerate(centroids):
-    patch_centers.append([index,value])
+    patch_centers.append([index+1,value])
 
-min_dist = np.inf
-landmarks = [[126.48541259765625, 124.46916198730469],
-  [179.53973388671875, 126.32745361328125],
-  [161.130126953125, 152.90260314941406],
-  [131.11929321289062, 186.91676330566406],
-  [176.72128295898438, 187.10568237304688]]
-
-for landmark in landmarks:
-    for i in len(patch_centers):
-        dist = np.linalg.norm(landmark,patch_centers[i,:])
+def calculate_landmark_patches(landmarks):
+    landmark_patches = []
+    center_patches = []
+    for j in range(5): # 5 is the number of landmarks found by mtcnn
+        min_dist = np.inf
+        # for i in range(len(patch_centers)):
+        #     diff = landmarks[0][j] - np.array(patch_centers[i][1])
+        #     dot_prod = np.dot(diff.T,diff)
+        #     dist = np.sqrt(dot_prod)
+        #     if dist < min_dist:
+        #         patch_temp = patch_centers[i][0]
+        #         min_dist = dist
+        for i in range(len(patch_centers)):
+            dist = np.linalg.norm(landmarks[0][j]-patch_centers[i][1])
+            if dist < min_dist:
+                patch_temp = patch_centers[i][0]
+                centroid_temp = patch_centers[i][1]
+                min_dist = dist
+        landmark_patches.append(patch_temp)
+        center_patches.append(centroid_temp)
+    return landmark_patches, center_patches

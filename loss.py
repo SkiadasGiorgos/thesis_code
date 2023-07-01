bf1ec13e91733f91f73f48ed688b879921fe5f88
SkiadasGiorgos <skiadas.giorgos04@gmail.com>
2023-07-01 20:55:32 +0300

    Create loss class

---
+++ loss.py
@@ -0,0 +1,24 @@
+import torch.nn as nn 
+import torch
+from timm.loss import LabelSmoothingCrossEntropy
+
+
+class Distillation_Loss(nn.Module):
+    def __init__(self,) :
+        super(Distillation_Loss,self).__init__()   
+        self.cross_entropy = LabelSmoothingCrossEntropy()
+        self.mse = nn.MSELoss()
+
+
+    def forward(self,output,target,features_old=None,features_new=None,distilled_old_feats=None,
+                distilled_new_feats=None):
+        if features_new is None:
+            loss = self.cross_entropy(output,target)
+        elif 0 in distilled_new_feats or 0 in distilled_old_feats:
+            loss = self.cross_entropy(output,target) + self.mse(features_new,features_old)
+        else:
+            loss = self.cross_entropy(output,target) + self.mse(features_new,features_old)+self.mse(distilled_new_feats,distilled_old_feats)
+        # else:
+        #     loss = self.cross_entropy(output,target) + self.mse(features_new,features_old)
+        return loss
+        
